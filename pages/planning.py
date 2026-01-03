9b2c1f142343a2ff7e25b982f83ee06f8b5f4273
wassannc <balaram@wassan.org>
2026-01-03 11:09:37 +0530

    Create planning.py

---
+++ pages/planning.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="RLV Village Planning", layout="wide")
+
+st.title("RLV – Village Planning & Budget Engine")
+st.info("This module will generate village / GP / Mandal wise development plans & budgets.")
