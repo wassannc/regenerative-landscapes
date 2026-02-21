d8d05f0b9006a8129805b34da6a6b116bc4a82b1
wassannc <balaram@wassan.org>
2026-02-21 10:50:34 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,6 +6,32 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
+
+# ---------- LOGIN CONFIG ----------
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status is False:
+    st.error("Username/password is incorrect")
+
+if authentication_status is None:
+    st.warning("Please enter your credentials")
+
+if not authentication_status:
+    st.stop()
+
+authenticator.logout("Logout", "sidebar")
+st.sidebar.write(f"Welcome {name}")
+
 st.set_page_config(
     page_title="RLV MEL Portal",
     layout="wide"
@@ -866,6 +892,7 @@ elif menu == "Natural Farming":
     
 
     
+
 
 
 
