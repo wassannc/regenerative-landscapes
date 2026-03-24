a387038c83c564948827def2dac9944175d9f5be
wassannc <balaram@wassan.org>
2026-03-24 14:14:25 +0530

    Update maps.py

--- pages/maps.py
+++ pages/maps.py
@@ -97,7 +97,12 @@ folium.GeoJson(
         "weight": 1,
         "fillColor": get_color(f["properties"].get("Land_Use", "")),
         "fillOpacity": 0.6,
-    }
+    },
+    tooltip=folium.GeoJsonTooltip(
+        fields=["Land_Use"],
+        aliases=["Type:"],
+        sticky=True
+    )
 ).add_to(land_layer)
 
 land_layer.add_to(m)
