236315f78de89abd88a72f9729f2d6dcbd903f68
wassannc <balaram@wassan.org>
2026-03-27 15:07:09 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -795,8 +795,7 @@ for col in numeric_cols:
     df_mig[col] = pd.to_numeric(df_mig[col], errors="coerce").fillna(0)
     df_mig.columns = df_mig.columns.str.strip()
     df_mig["Average earning per annum per family"] = pd.to_numeric(
-    df_mig["Average earning per annum per family"], errors="coerce"
-    )
+    
     mandal = st.selectbox("Select Mandal",["All"]+sorted(df_mig["mandal"].unique()))
     if mandal!="All":
         df_mig = df_mig[df_mig["mandal"]==mandal]
